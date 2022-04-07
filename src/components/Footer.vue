cb30ebaf0fe01222a3a478c4f55b19c311b98f47
Partithura <partithura@pop-os.localdomain>
2022-04-07 08:57:21 -0300

    💄 Inserção da footer e ajuste na page cadastros.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,52 @@
 <template>
   <footer id="footer">
     <img src="../../public/logo-branco.png" />
-    <h1>Contato</h1>
-    <span>E-mail: contato@elyte.ind.br</span> 
-    <span>Telefone:(55)9 9886-7720</span><br>
-    <span>Desenvolvimento: Departamento de TI - Fundimisa © 2020</span> <br>
+    <span class="span-ceredits">Desenvolvimento: Departamento de TI - Fundimisa © 2020</span> <br>
+    <div class="footer-box"> 
+      <div id="sow-editor-3" class="footer-widget">
+        <div class="swidget-editor-base">
+          <div class="siteorigin-widget">
+            <h3 style="text-align: right;">
+              <span style="text-align: right;">Localização:</span>
+            </h3>
+            <p>
+              <br>
+              Rodovia BR 285, km 503,5, s/n°
+              <br>
+              Bairro Interior
+              <br>
+              Cidade: Entre-Ijuis/RS
+              <br>
+              CEP:98.855-000
+              <br>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="footer-box1"> 
+      <div id="sow-editor-2" class="footer-widget1">
+        <div class="swidget-editor-base1">
+          <div class="siteorigin-widget1">
+            <h3 style="text-align: left;">
+              <span style="text-align: left;">Contato:</span>
+            </h3>
+            <p>
+              <br>
+              Rodovia BR 285, km 503,5, s/n°
+              <br>
+              Bairro Interior
+              <br>
+              Cidade: Entre-Ijuis/RS
+              <br>
+              CEP:98.855-000
+              <br>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </footer>
 </template>
 
@@ -20,10 +62,18 @@ export default {
   height: 200px;
   background-color: #222;
   color: #fff;
-  text-align: center;
   display: flex;
   align-items: center;
   justify-content: center;
   
+}
+
+#sow-editor-3{
+   text-align: right;
+}
+
+.span-ceredits {
+  text-align: end;
+  color: rgb(255, 255, 255);
 }
 </style>
